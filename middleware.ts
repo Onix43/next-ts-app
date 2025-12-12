b2394f07a1b9078a3df6eb60787405c1a03df879
Onix <piyvoda20033@gmail.com>
2025-12-12 19:19:17 +0200

    added token auth

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,16 @@ import jwt from 'jsonwebtoken';
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl; // pathname які будемо пропускати далі...
   const token = req.cookies.get('token')?.value;
 
+  if (pathname.startsWith('/login') || pathname.startsWith('/register')) {
+    return NextResponse.next();
+  }
   if (!token) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
+
   try {
     jwt.verify(token, 'SECRET');
     return NextResponse.next();
@@ -16,5 +21,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard'],
+  matcher: ['/dashboard', '/login', '/register'],
 };
