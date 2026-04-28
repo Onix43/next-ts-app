00763b614161246086446a6288790683f8adfbee
Onix <piyvoda20033@gmail.com>
2026-04-28 18:13:27 +0300

    interview tasks

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,35 +1,44 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-interface User {
-  id: number;
-  name: string;
-}
-
-const users: User[] = [
-  { id: 1, name: 'Ivan' },
-  { id: 2, name: 'Petro' },
-  { id: 3, name: 'Olga' },
-  { id: 4, name: 'Andrii' },
-];
+const fakeFetch = (query: string) =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve([`${query}-1`, `${query}-2`, `${query}-3`]);
+    }, Math.random() * 1000);
+  });
 
 export default function TodoList() {
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState<boolean>(false);
-  const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
+  const [result, setResult] = useState<string[]>([]);
 
   const timerId = useRef<ReturnType<typeof setTimeout> | null>(null);
   const debouncedSearch = (value: string) => {
-    setLoading(true);
     if (timerId.current) clearTimeout(timerId.current);
 
+    setLoading(true);
     timerId.current = setTimeout(() => {
       setQuery(value);
-      setLoading(false);
-    }, 500);
+    }, 300);
   };
-  const filteredUsers = users.filter(user =>
-    user.name.toLowerCase().includes(query.toLowerCase())
-  );
+
+  useEffect(() => {
+    let isActual = true;
+
+    const asyncFunction = async () => {
+      const fetchRes = (await fakeFetch(query)) as unknown as string[];
+      if (isActual) {
+        setResult(fetchRes);
+      }
+      setLoading(false);
+    };
+    asyncFunction();
+
+    return () => {
+      isActual = false;
+    };
+  }, [query]);
+
   return (
     <div>
       <input
@@ -38,28 +47,7 @@ export default function TodoList() {
         onChange={e => debouncedSearch(e.target.value)}
       />
       {loading && <p>Loading, please wait...</p>}
-      {!loading && filteredUsers.length === 0 && (
-        <p>Sorry, no results matched your call</p>
-      )}
-      <ul>
-        {filteredUsers.map(user => (
-          <li
-            key={user.id}
-            onClick={() => {
-              if (selectedUsers.includes(user)) {
-                setSelectedUsers(prev =>
-                  prev.filter(item => item.id !== user.id)
-                );
-              } else setSelectedUsers(prev => [...prev, user]);
-            }}
-          >
-            <h3>{user.name}</h3>
-            {selectedUsers.includes(user)
-              ? 'Delete from selected❌'
-              : 'Mark as selected✅'}
-          </li>
-        ))}
-      </ul>
+      {result && result.map(res => <li key={res}>{res}</li>)}
     </div>
   );
 }
